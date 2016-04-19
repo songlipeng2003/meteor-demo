e2e56236eaf73fcc917495ef3b9cb54bc80b4df0
Thinking Song <123wuming_123@163.com>
2016-04-19 12:31:52 +0800

    add angular to to list

--- client/main.js
+++ client/main.js
@@ -1,22 +1,18 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+import todosList from '../imports/components/todosList/todosList';
 
-import './main.html';
+angular.module('simple-todos', [
+  angularMeteor,
+  todosList.name
+]);
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+function onReady() {
+  angular.bootstrap(document, ['simple-todos']);
+}
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+if (Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+} else {
+  angular.element(document).ready(onReady);
+}
